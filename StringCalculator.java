70ed3a371d8c2991caca7967212cad858175c2ed
noamanvora <nvie789@gmail.com>
2020-11-12 18:09:40 +0530

    commit string calculator task with validation of negative number in input

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
- 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 	public static void main(String[] args) {
 
@@ -8,28 +12,42 @@ public class StringCalculator {
 		String str = sc.nextLine();
 		StringCalculator noaman = new StringCalculator();
 		int ans = noaman.add(str);
-		System.out.println(ans);
+		System.out.println("Total of Positive Numbers : " + ans);
 	}
 
-	public int add(String number)
-	{
-		int total=0;
+	public int add(String number) {
+		int total = 0;
+		List<String> negativeNums = new ArrayList<String>();
 		try {
 
 			Pattern p = Pattern.compile("(-?[0-9]+)");
 			Matcher m1 = p.matcher(number);
-			ArrayList<Integer> list = new ArrayList<Integer>();
+			ArrayList<Integer> positiveNums = new ArrayList<Integer>();
 
 			while (m1.find()) {
-					list.add(Integer.parseInt(m1.group()));
+				boolean isNegative = checknegative(m1.group());
+				if (isNegative) {
+					negativeNums.add(m1.group());
+				} else {
+					positiveNums.add(Integer.parseInt(m1.group()));
+				}
 			}
-			
-			for (Integer n : list) {
+
+			for (Integer n : positiveNums) {
 				total = total + n;
 			}
-		} catch (NegativeNumberException n) {
-			System.out.println(n);
+			System.out.println("Total Positive numbers in input : " + positiveNums);
+			if (negativeNums.size() > 0) {
+				throw new Exception();
+			}
+		} catch (Exception n) {
+			System.out.println(
+					"Negative Numbers are not allowed. You have input below negative number : \n" + negativeNums);
 		}
 		return total;
+	}
+
+	public boolean checknegative(String num) {
+		return Integer.parseInt(num) < 0;
 	}
 }
